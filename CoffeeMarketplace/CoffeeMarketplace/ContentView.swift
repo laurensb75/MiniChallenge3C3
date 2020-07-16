f5577d5f275db8c380ac4c394df98c925cbb32f3
Laurens <laurensb75@gmail.com>
2020-07-16 17:55:31 +0700

    initial project using SwiftUI

---
+++ CoffeeMarketplace/CoffeeMarketplace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeMarketplace
+//
+//  Created by Laurens Bryan Cahyana on 16/07/20.
+//  Copyright © 2020 laurens bryan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
